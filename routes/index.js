2b9f8f44a1e975cefc696f005f578faa4be8aaae
Gwyn Jones <m@gwnjns.com>
2017-12-12 14:43:51 +0000

    Add research guides
    This is a basic list view and the ability to search by name.

--- routes/index.js
+++ routes/index.js
@@ -15,14 +15,7 @@ db()
 db()
     .then(() => {
         db.ResearchGuide.populate(decorated_guides)
-    })
-    .then(() => {
-        db.ResearchGuide.find_guides_with_name_containing('marriage')
-            .then(guides => {
-                console.log(guides);
-            })
     });
-
 
 fs.readdir("public/data", (err, files) => {
     if (err) {
@@ -40,7 +33,27 @@ router.get('/department/', (req, res) => {
 });
 
 router.get('/department/:id', (req, res) => {
-    res.render('department', { department: req.params.id, codes: department_codes });
+    res.render('department', { department: req.params.id, codes: department_codes, guides: guides });
+});
+
+router.get('/guides/', (req, res) => {
+
+    db().then(() => {
+        db.ResearchGuide.all()
+            .then(guides => {
+                res.render('guides', { guides: guides });
+            })
+    });
+});
+
+router.get('/guide/', (req, res) => {
+
+    db().then(() => {
+        db.ResearchGuide.find_guides_with_name_containing(req.query.name)
+            .then(guides => {
+                res.render('guides', { guides: guides });
+            })
+    });
 });
 
 module.exports = router;
